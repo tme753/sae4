25734c27a1cc2f0920b8c2aa6ea30e4ddbfca4d5
tme753 <tom.morand@etu.univ-st-etienne.fr>
2025-01-20 00:42:54 +0100

    avancement du weekend

--- src/Controller/TomController.php
+++ src/Controller/TomController.php
@@ -7,10 +7,46 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TomController extends AbstractController{
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('/home', name: 'app_accueil')]
     public function acceuil(): Response
     {
         return $this->render('tom/accueil.html.twig', [
+            'name' => 'Tom',
+            'first_name' => 'Morand',
+            'groupe_tp' => 'Groupe B1',
+        ]);
+    }
+    #[Route('/plus', name: 'app_plus')]
+    public function plus(): Response
+    {
+        return $this->render('tom/plus.html.twig', [
+            'name' => 'Tom',
+            'first_name' => 'Morand',
+            'groupe_tp' => 'Groupe B1',
+        ]);
+    }
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('tom/cv.html.twig', [
+            'name' => 'Tom',
+            'first_name' => 'Morand',
+            'groupe_tp' => 'Groupe B1',
+        ]);
+    }
+    #[Route('/Me_contacter', name: 'app_Me_contacter')]
+    public function Me_contacter(): Response
+    {
+        return $this->render('tom/Me_contacter.html.twig', [
+            'name' => 'Tom',
+            'first_name' => 'Morand',
+            'groupe_tp' => 'Groupe B1',
+        ]);
+    }
+    #[Route('/portfolio', name: 'app_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('tom/portfolio.html.twig', [
             'name' => 'Tom',
             'first_name' => 'Morand',
             'groupe_tp' => 'Groupe B1',
